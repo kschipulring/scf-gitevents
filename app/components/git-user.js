5ad7f90908b50422edb33d3895ff1d19f82711ef
kschipulring <karl@3ringprototype.com>
2017-10-25 11:10:04 -0400

    finalized git-users main routing and linking

--- app/components/git-user.js
+++ app/components/git-user.js
@@ -1,28 +1,13 @@
 import Component from '@ember/component';
-//import DS from 'ember-data';
 import $ from 'jquery';
 
 export default Component.extend({
-	//users: Ember.inject.service(),
-	
 	didInsertElement() {
 		this._super(...arguments);
-		/*this.get('users').userList().then((users) => {
-			this.set('users', users);
-		});*/
-
-		//console.log( 'DS.Store = ', DS.Store.findAll("users") );
-		//console.log( 'DS = ', DS );
-		
-		//this.set('users',  this.store.findAll("users") );
 	},
 	actions: {
-		/*chooseUser(user) {
-			//jQuery, because it gets to the point
-			$("#git-users li").removeClass("active");
-			$("#users_"+user.id).addClass("active");
-			
-			
-		}*/
+		chooseUser(user) {
+
+		}
 	}
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,7 @@ import Route from '@ember/routing/route';
 const { set } = Ember;
 
 export default Route.extend({
-	model() {
-		return this.store.findAll('git-user');
-	},
-
-	setupController(controller, model) {
-		set(controller, 'gitUsers', model );
+	beforeModel() {
+		this.transitionTo('git-users');
 	}
 });
